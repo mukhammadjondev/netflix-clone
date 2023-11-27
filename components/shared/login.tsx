7bf9260a7db393dec30b9f067d55097e9094542a
mukhammadjondev <muhsdev@gmail.com>
2023-11-27 16:35:56 +0500

    account form

--- components/shared/login.tsx
+++ components/shared/login.tsx
@@ -7,7 +7,7 @@ const Login = () => {
   return (
     <div className="w-full h-screen">
       <div className="absolute inset-0">
-        <Image src="https://repository-images.githubusercontent.com/299409710/b42f7780-0fe1-11eb-8460-e459acd20fb4" alt="bg" fill />
+        <Image src="https://t.ly/d7zU1" alt="bg" fill />
       </div>
       <div className="relative z-10 rounded-md w-[500px] h-[50vh] bg-black/60 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 px-8 py-4">
         <div className="flex h-full justify-center items-center">
--- components/shared/manage-account.tsx
+++ components/shared/manage-account.tsx
@@ -1,6 +1,64 @@
+import { Trash2, LockKeyhole } from "lucide-react"
+import Image from "next/image"
+import { useState } from 'react'
+import CreateAccountForm from "../form/create-account-form"
+import LoginAccountForm from "../form/login-account-form"
+import { Button } from "../ui/button"
+import { Dialog, DialogContent } from "../ui/dialog"
+
 const ManageAccount = () => {
+  const [isDelete, setIsDelete] = useState<boolean>(false)
+  const [open, setOpen] = useState<boolean>(false)
+  const [state, setState] = useState<'login' | 'create'>('create')
+
   return (
-    <div>Manage Account</div>
+    <div className="min-h-screen flex flex-col justify-center items-center relative">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-white text-5xl font-bold my-10 text-center">Who's Watching?</h1>
+
+        <ul className="flex p-0 my-10 gap-6 flex-col sm:flex-row">
+          <li onClick={() => {
+              setOpen(true)
+              setState('login')
+            }} className="max-w-[220px] w-[155px] cursor-pointer flex flex-col items-center gap-3 rounded">
+            <div className="relative">
+              <div className="max-w-[200px] min-w-[84px] max-h-[200px] min-h-[84px] rounded object-cover w-[155px] h-[155px] relative">
+                <Image src='https://t.ly/9eB20' alt="account" fill className="rounded" />
+              </div>
+              {isDelete ? (
+                <div className="absolute transform z-10 bottom-0 cursor-pointer">
+                  <Trash2 className="w-8 h-8 text-red-600" />
+                </div>
+              ) : null}
+            </div>
+
+            <div className="flex items-center gap-1">
+              <span className="font-mono font-bold text-xl">Mukhammadjon</span>
+              <LockKeyhole />
+            </div>
+          </li>
+
+          <li onClick={() => {
+              setOpen(true)
+              setState('create')
+            }}
+            className={"border bg-[#e5b109] font-bold text-xl border-black max-w-[200px] rounded min-w-[84px] max-h-[200px] min-h-[84px] w-[155px] h-[155px] cursor-pointer flex justify-center items-center"} >
+              Add account
+          </li>
+        </ul>
+
+        <Button onClick={() => setIsDelete(prev => !prev)} className={"bg-transparent rounded-none hover:bg-transparent !text-white border border-gray-100 cursor-pointer tracking-wide inline-flex text-sm px-[1.5em] py-[0.5em] mb-10"}>
+          Manage Profiles
+        </Button>
+      </div>
+
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          {state === 'create' && <CreateAccountForm />}
+          {state === 'login' && <LoginAccountForm />}
+        </DialogContent>
+      </Dialog>
+    </div>
   )
 }
 
